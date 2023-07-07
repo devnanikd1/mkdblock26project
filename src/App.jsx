06af31caf078ad0f85afc3f0d5d172aa8e93572c
Mamta <mamtakdev@gmail.com>
2023-07-07 19:12:23 -0400

    contact updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import ContactList from './components/ContactList'
-
-
-
+import SelectedContact from "./components/SelectedContact";
 export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <SelectedContact setSelectedContactId={
+          setSelectedContactId} selectedContactId ={selectedContactId}/>
+      ) : (
+       <ContactList setSelectedContactId={setSelectedContactId}  />
+      )}
     </>
   );
 }
---
+++ src/components/SelectedContact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+export default function SelectedContact({  selectedContactId, setSelectedContactId }){
+    const [contact, setContact] = useState(null)
+    useEffect(() => {
+        async function fetchContact() {
+          try {
+            const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`)              
+            const result = await response.json();
+            setContact(result);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        fetchContact()
+      }, []);
+    console.log (contact);
+    console.log (selectedContactId);
+    return (
+    <div>
+        {contact && 
+        <ul>
+        <li>{contact.address.street}</li>
+        <li>{contact.address.city}</li>
+        <li>{contact.email}</li>
+        <li>{contact.phone}</li>
+      </ul>}
+    </div>)
+} 
+
+
